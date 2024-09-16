d5bdc66e5cb2fbd98fbdc063123c44b883a99391
Angel <apuentest@uniempresarial.edu.co>
2024-09-16 14:24:19 -0500

    Actualización de la clase main

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenido(a) a ScreenMatch");
         System.out.println("El recuerdo de Marnie");
-        System.out.println("Hola");
+        System.out.println("Ya no quiero saludar a nadie");
     }
 }
