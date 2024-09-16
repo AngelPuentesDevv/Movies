a43d2e6e36d441044d7c49e54193e2d1fa7684f6
unknown <apuentest@uniempresarial.edu.co>
2024-09-16 12:15:07 -0500

    Creación de la clase main

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido(a) a ScreenMatch");
+        System.out.println("El recuerdo de Marnie");
+    }
+}
