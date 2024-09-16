aca206040a03274b5dcc5918e8c83a1adf15c408
Angel <apuentest@uniempresarial.edu.co>
2024-09-16 14:52:13 -0500

    Restauración clase main

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenido(a) a ScreenMatch");
         System.out.println("El recuerdo de Marnie");
-        System.out.println("Ya no quiero saludar a nadie");
     }
 }
